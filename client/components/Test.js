c0a26cadfcd41a9736ea7a181ccec6901de14d3b
Mike <troianello@protonmail.com>
2020-02-26 23:41:44 -0800

    Added backend. Connects via local ip address

--- client/components/Test.js
+++ client/components/Test.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class Test extends Component {
+  state = {
+    fetchMsg: 'fetching...'
+  };
+
+  componentDidMount() {
+    console.log('INSIDE TEST');
+    fetch('http://IP.ADDRESS/test')
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log('blsh', responseJson.message);
+        this.setState({
+          fetchMsg: responseJson.message
+        });
+      })
+      .catch(function(error) {
+        console.log(
+          'There has been a problem with your fetch operation: ' + error.message
+        );
+        throw error;
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>TEST</Text>
+        <Text>{this.state.fetchMsg}</Text>
       </View>
     );
   }
